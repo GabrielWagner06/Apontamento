9cb24775f7234403959d885ce95586965ff4d4d3
GabrielWagner06 <gabrielwagnerzimmermann@gmail.com>
2024-09-02 16:44:51 -0300

    Fix professor

---
+++ src/config/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    containerInner: {
+        padding: 20,
+    },
+    selfStretch: {
+        alignSelf: "stretch",
+
+    },
+})
+
+export default styles;
---
+++ src/screens/LoginScreen.jsx
@@ -0,0 +1,12 @@
+import { Surface, Text } from "react-native-paper";
+import styles from "../config/styles";
+
+export default function LoginScreen() {
+  return (
+    <Surface
+        style={styles.container}
+    >
+      <Text>Tela de Login</Text>
+    </Surface>
+  );
+}
